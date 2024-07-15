f618c44c51e59b2b8f651ecfee70204455b5ba73
deops-adnan <aap090400@gmail.com>
2024-07-15 12:40:26 +0530

    config changes

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_ONdkyttEd', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '4epph79ntvhvvmj4rc9k8tem2a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
