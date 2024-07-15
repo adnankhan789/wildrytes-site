99fbeaa377adc84330113fb69b5ac715000666e1
deops-adnan <aap090400@gmail.com>
2024-07-15 12:39:07 +0530

    config changes

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_ONdkyttEd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4epph79ntvhvvmj4rc9k8tem2a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
